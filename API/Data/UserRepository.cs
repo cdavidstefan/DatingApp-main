ffcb9c779afb41b5ae8a74f79f2b28f3ae31b488
David Stefan <ciucadavidstefan@gmail.com>
2024-01-07 10:44:24 +0200

    bigNastyErrors_toSolve

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -33,11 +33,6 @@ namespace API.Data
             return await _context.Users.Include(p => p.Photos).ToListAsync();
         }
 
-        public async Task<bool> SaveAllAsync()
-        {
-            return await _context.SaveChangesAsync() > 0;
-        }
-
         public void Update(AppUser user)
         {
             _context.Entry(user).State = EntityState.Modified;
@@ -75,5 +70,11 @@ namespace API.Data
             .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
             .SingleOrDefaultAsync();
         }
+
+        public async Task<string> GetUserGender(string username)
+        {
+            return await _context.Users.Where(x => x.UserName == username).Select(x => x.Gender).FirstOrDefaultAsync();
+        }
+
     }
 }
